77eee561f78d1e77aa8705286e04ab1d9cc3c73f
Maxim <maks.nasonov.03@gmail.com>
2025-06-07 00:45:17 +0700

    add sqlalchemy models

---
+++ src/users/models.py
@@ -0,0 +1,31 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, Text, Float
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    
+    id = Column(Integer, primary_key=True)
+    login = Column(String)
+    name = Column(String)
+    password_hash = Column(String)
+    is_admin = Column(Boolean)
+    
+    ads = relationship("Ad", back_populates="user")
+
+
+
+class Ad(Base):
+    __tablename__ = 'ad'
+    
+    id = Column(Integer, primary_key=True)
+    type = Column(String, default="Продажа") # В будущем добавить Enum
+    name = Column(String)
+    description = Column(Text)
+    price = Column(Float)
+    image = Column(String)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    
+    user = relationship("User", back_populates="ads")
