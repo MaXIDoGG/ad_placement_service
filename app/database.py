bd4a4cf3ce4a7de677ca1963f28a19c25531fc94
Maxim <maks.nasonov.03@gmail.com>
2025-06-09 01:43:36 +0700

    delete Base.metadata.drop_all string

--- app/database.py
+++ app/database.py
@@ -7,7 +7,6 @@ from config import ADMIN_LOGIN, ADMIN_NAME, ADMIN_PASSWORD
 
 async def init_models():
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 async def create_admin():
